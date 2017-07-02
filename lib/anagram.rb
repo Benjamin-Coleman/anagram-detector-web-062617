bb93ec2899f63f674dc7cde1aa171d6f62cf817c
Benjamin-Coleman <github email address>
2017-07-02 15:29:23 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+class Anagram
+
+	attr_accessor :word, :split_word
+
+	def initialize(word)
+		@word = word
+		@split_word = word.split("").sort
+	end
+
+	def match(array)
+		split = []
+		array.each do |word|
+			split << word.split("")
+		end
+		split.select {|word| word.sort == @split_word}.map{ |letters| letters.join}
+	end
+
+end
